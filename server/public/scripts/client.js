b8ff88be557ec9e2521f72bd9786f6db0e5defad
mbmosman <mbMosman@users.noreply.github.com>
2018-02-14 10:39:00 -0600

    after Wed lecture

--- server/public/scripts/client.js
+++ server/public/scripts/client.js
@@ -3,13 +3,13 @@ $(document).ready(onReady);
 function onReady(){
   console.log('Hello');
 
-  addSong({
-    artist: 'John Rule', 
-    track: 'Always on Time', 
-    published: '1/1/1999', 
-    rank: 2
-  });
-
+  // addSong({
+  //   artist: 'John Rule', 
+  //   track: 'Always on Time', 
+  //   published: '1/1/1999', 
+  //   rank: 2
+  // });
+}
   function addSong(song) {
     $.ajax({
       type: 'POST',
@@ -24,5 +24,29 @@ function onReady(){
     })
   }
 
+  function updateSongRating(id, newRating) {
+    $.ajax({
+      type: 'PUT',
+      url: `/songs/${id}`,
+      data: { rating: newRating }
+    })
+    .done(function (response) {
+      console.log('Updated song rating');
+    })
+    .fail(function (error){
+      console.log(error);
+    })
+  }
 
-}
+  function deleteSong(id){
+    $.ajax({
+      type: 'DELETE',
+      url: `songs/${id}`,
+    })
+    .done(function (response){
+      console.log('Deleted song');
+    })
+    .fail(function(error) {
+      console.log(error);
+    })
+  }
--- server/routers/songs-router.js
+++ server/routers/songs-router.js
@@ -19,6 +19,54 @@ router.get('/', function(request, response){
     })
 })
 
+// Get a single song from /songs/5, where 5 is the id
+router.get('/:id', (request, response) => {
+  const id = request.params.id;
+  const sqlText = 'Select * FROM songs WHERE id=$1';
+  pool.query(sqlText, [ id ])
+    .then((result) => {
+      console.log(`Getting song ${id}`);
+      response.send(result.rows);
+    })
+    // bad things could happen...
+    .catch(function(error){
+      console.log(`Error on Get song ${id}:`, error);
+      response.sendStatus(500);
+    })
+})
+
+// Delete a single song from /songs/5, where 5 is the id
+router.delete('/:id', (request, response) => {
+  const id = request.params.id;
+  const sqlText = 'DELETE FROM songs WHERE id=$1';
+  pool.query(sqlText, [ id ])
+    .then((result) => {
+      console.log(`Deleted song ${id}`);
+      response.sendStatus(200);
+    })
+    // bad things could happen...
+    .catch(function(error){
+      console.log(`Error on Delete song ${id}:`, error);
+      response.sendStatus(500);
+    })
+})
+
+// Update the rating of a specific song
+router.put('/:id', (request, response) => {
+  const id = request.params.id;
+  const newRating = request.body.rating;
+  const sqlText = `UPDATE songs SET rank=$1 WHERE id=$2`;
+  pool.query(sqlText, [newRating, id])
+    .then((result) => {
+      console.log(`Updated song ${id} with rank ${newRating}`);
+      response.sendStatus(200);
+    })
+    .catch( (error) => {
+      console.log('Error on update song');
+      response.sendStatus(500);
+    })
+})
+
 router.post('/add', (request, response) => {
   const song = request.body;
   console.log('Add song:', song);
